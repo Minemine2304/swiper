b369bdae3db7d48b143b5b917cef86964912304d
xiaoyaowan456 <675235295@qq.com>
2020-09-24 20:23:08 +0800

    1.添加了短信发送模块 2.完成发送验证码接口

---
+++ swiper/config.py
@@ -0,0 +1,8 @@
+'''程序逻辑配置，及第三方平台配置'''
+
+# 赛迪云通信设置
+SD_APPID = '54821'
+SD_APPKEY = 'df52f82208c6945297041767b58fe667'
+SD_PROJECT= 'qJyAp3' # 短信模板的ID
+SD_SIGN_TYPE= 'md5'
+SD_API = 'https://api.mysubmail.com/message/xsend.json'
